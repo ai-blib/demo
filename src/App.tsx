d0c4b924b2c295bdf17f00e23cacaddfded3148f
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-22 05:32:27 +0800

    code updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
 import {
   AreaLight,
-  Cube,
   Dummy,
-  LingoEditor,
   Model,
-  OrbitCamera,
   Reflector,
   Setup,
   SvgMesh,
   ThirdPersonCamera,
   World,
-  types
+  types,
 } from "lingo3d-react";
 import { useCallback, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [arrowPosition, setArrowPosition] = useState({ x: 0, y: 0, z: 0 })
-  const dummyRef = useRef<types.Dummy>(null)
+  const [arrowPosition, setArrowPosition] = useState({ x: 0, y: 0, z: 0 });
+  const dummyRef = useRef<types.Dummy>(null);
+  const [running, setRunning] = useState(false);
 
-  const handleClick = useCallback((e: types.LingoMouseEvent) => {
-    const dummy = dummyRef.current
-    if (!dummy) return
+  const handleClick = useCallback((e: types.MouseEvent) => {
+    const dummy = dummyRef.current;
+    if (!dummy) return;
 
-    setArrowPosition(e.point)
-    dummy.lerpTo(e.point.x, dummy.y, e.point.z, 0.01)
+    setArrowPosition(e.point);
+    dummy.lookTo(e.point.x, undefined, e.point.z, 0.1);
+    dummy.moveTo(e.point.x, undefined, e.point.z, 10);
+    setRunning(true);
 
-    dummy.onLoop = () => {
-      dummy.lookAt({ ...e.point, y: dummy.y })
-    }
-  }, [])
+    dummy.onMoveToEnd = () => {
+      setRunning(false);
+    };
+  }, []);
 
   return (
     <World>
       <Model
+        physics="map"
         bloom
         width={245.36}
         depth={245.36}
@@ -43,11 +44,13 @@ const App = () => {
         src="scene.glb"
         onClick={handleClick}
       />
-      <ThirdPersonCamera active mouseControl="drag" lockTargetRotation="dynamic-lock"  >
+      <ThirdPersonCamera active mouseControl="drag" lockTargetRotation={false}>
         <Dummy
+          physics="character"
           ref={dummyRef}
           metalnessFactor={0.5}
           roughnessFactor={0.5}
+          animation={running ? "running" : "idle"}
           y={44.58}
         />
       </ThirdPersonCamera>
@@ -69,31 +72,32 @@ const App = () => {
         intensity={2.6}
         helper={false}
       />
-      <SvgMesh
-        bloom
-        metalnessFactor={1}
-        roughnessFactor={0.4}
-        width={72.99}
-        height={100}
-        depth={100}
-        scaleX={-0.82}
-        scaleZ={0.19}
-        src="arrow.svg"
-        color="#ff4e4e"
-        emissiveColor="#223056"
-        roughness={0.4}
-        x={arrowPosition.x}
-        y={arrowPosition.y + 50}
-        z={arrowPosition.z}
-        animation={{ rotationY: [0, 45, 90, 135, 180, 225, 270, 315, 360] }}
-      />
+      {running && (
+        <SvgMesh
+          bloom
+          metalnessFactor={1}
+          roughnessFactor={0.4}
+          width={72.99}
+          height={100}
+          depth={100}
+          scaleX={-0.82}
+          scaleZ={0.19}
+          src="arrow.svg"
+          color="#ff4e4e"
+          emissiveColor="#223056"
+          roughness={0.4}
+          x={arrowPosition.x}
+          y={arrowPosition.y + 50}
+          z={arrowPosition.z}
+          animation={{ rotationY: [0, 45, 90, 135, 180, 225, 270, 315, 360] }}
+        />
+      )}
       <Setup
         defaultFog="white"
         defaultLight={false}
         bloomStrength={1}
         bloomThreshold={0.5}
       />
-      {/* <LingoEditor/> */}
     </World>
   );
 };
