3ffc2015c79547ae52c97e0676cf886c3ad9fefb
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-07-11 01:13:29 +0800

    code updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,46 @@ import {
   Dummy,
   Model,
   Reflector,
-  Setup,
   SvgMesh,
   ThirdPersonCamera,
   World,
   types,
   usePreload,
-  useWindowSize,
-} from "lingo3d-react";
-import { useRef, useState } from "react";
-import "./App.css";
+} from "lingo3d-react"
+import { useRef, useState } from "react"
+import "./App.css"
 
+// Game world component
+// 场景组件
 const Game = () => {
-  const [arrowPosition, setArrowPosition] = useState({ x: 0, y: 0, z: 0 });
-  const dummyRef = useRef<types.Dummy>(null);
-  const [running, setRunning] = useState(false);
-  const { width } = useWindowSize();
+  // When user clicks on map model, set arrow position to clicked position
+  // 点击地图模型时，设置箭头位置为点击位置
+  const [arrowPosition, setArrowPosition] = useState({ x: 0, y: 0, z: 0 })
+  
+  const dummyRef = useRef<types.Dummy>(null)
+  const [running, setRunning] = useState(false)
 
   const handleClick = (e: types.MouseEvent) => {
-    const dummy = dummyRef.current;
-    if (!dummy) return;
+    const dummy = dummyRef.current
+    if (!dummy) return
 
-    setArrowPosition(e.point);
-    dummy.lookTo(e.point.x, undefined, e.point.z, 0.1);
-    dummy.moveTo(e.point.x, undefined, e.point.z, 10);
-    setRunning(true);
+    setArrowPosition(e.point)
+    dummy.lookTo(e.point.x, undefined, e.point.z, 0.1)
+    dummy.moveTo(e.point.x, undefined, e.point.z, 10)
+    setRunning(true)
 
     dummy.onMoveToEnd = () => {
-      setRunning(false);
-    };
-  };
+      setRunning(false)
+    }
+  }
 
   return (
-    <World>
+    <World
+      defaultFog="white"
+      defaultLight="env1.jpg"
+      bloomStrength={1}
+      bloomThreshold={0.5}
+    >
       <Model
         physics="map"
         bloom
@@ -43,23 +50,28 @@ const Game = () => {
         scaleX={20}
         scaleY={20}
         scaleZ={20}
-        src="scene.glb"
+        src="stadium.glb"
         onClick={handleClick}
+      />
+      <Reflector
+        y={-39.38}
+        scaleX={113.2}
+        scaleY={113.2}
+        rotationX={-90}
+        blur={512}
       />
       <ThirdPersonCamera
         active
         mouseControl="drag"
         lockTargetRotation={false}
-        fov={width < 640 ? 110 : 90}
         enableDamping
       >
         <Dummy
           physics="character"
           ref={dummyRef}
-          metalnessFactor={0.5}
-          roughnessFactor={0.5}
+          metalnessFactor={-0.5}
+          roughnessFactor={0}
           animation={running ? "running" : "idle"}
-          bloom
           y={44.58}
         />
       </ThirdPersonCamera>
@@ -83,28 +95,21 @@ const Game = () => {
           animation={{ rotationY: [0, 45, 90, 135, 180, 225, 270, 315, 360] }}
         />
       )}
-      <Setup
-        defaultFog="white"
-        defaultLight="env1.jpg"
-        bloomStrength={1}
-        bloomThreshold={0.5}
-        antiAlias="SSAA"
-      />
     </World>
-  );
-};
+  )
+}
 
 const App = () => {
-  const progress = usePreload(["scene.glb"], "1.2mb");
+  const progress = usePreload(["stadium.glb"], "1.2mb")
 
   if (progress < 100)
     return (
       <div className="w-screen h-screen absolute left-0 top-0 text-white bg-black flex items-center justify-center">
         loaded: {Math.round(progress)}%
       </div>
-    );
+    )
 
-  return <Game />;
-};
+  return <Game />
+}
 
-export default App;
+export default App
